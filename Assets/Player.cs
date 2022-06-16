9305ee97ea8da1955e238545f1134fbe6352858f
sun <sun@codebrick.co>
2022-06-17 03:19:25 +0900

    init

---
+++ Assets/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    Vector2 rawInput;
+
+    void Update()
+    {
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+    }
+}
