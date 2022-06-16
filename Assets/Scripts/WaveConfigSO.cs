1d2298375436229bd5c3b58863715cc5fc2aa149
sun <sun@codebrick.co>
2022-06-17 04:13:46 +0900

    Define enemy path

--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(MENUNAME = "Wave Config", fileName = "New Wave Config")]
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
     [SerializeField] Transform pathPrefab;
