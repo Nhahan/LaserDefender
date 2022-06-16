eb776767e19119f754ef2d9b4eedb51ce1f84e9d
sun <sun@codebrick.co>
2022-06-17 03:54:27 +0900

    init

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(MENUNAME = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+
+}
